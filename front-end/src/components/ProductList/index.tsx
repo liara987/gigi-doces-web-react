951ea3d6e42635ad972df4d2e926eeed7a4b6eb0
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2023-04-21 18:10:07 -0300

    fix: Fixing database and reading data through loop

--- front-end/src/components/ProductList/index.tsx
+++ front-end/src/components/ProductList/index.tsx
@@ -1,56 +1,13 @@
-/* eslint-disable no-octal */
 import "./index.scss";
-import data from '../../../database.json';
+import data from "../../../database.json";
 import Card from "../Card/index.tsx";
 
 function ProductList() {
   return (
-    <main className="ProductList">      
-      <Card
-        src={data.product01.image}
-        cost={data.product01.cost}
-        title={data.product01.title}
-      />
-      <Card
-        src={data.product02.image}
-        cost={data.product02.cost}
-        title={data.product02.title}
-      />
-      <Card
-        src={data.product03.image}
-        cost={data.product03.cost}
-        title={data.product03.title}
-      />
-      <Card
-        src={data.product04.image}
-        cost={data.product04.cost}
-        title={data.product04.title}
-      />
-      <Card
-        src={data.product05.image}
-        cost={data.product05.cost}
-        title={data.product05.title}
-      />
-      <Card
-        src={data.product06.image}
-        cost={data.product06.cost}
-        title={data.product06.title}
-      />
-      <Card
-        src={data.product07.image}
-        cost={data.product07.cost}
-        title={data.product07.title}
-      />
-      <Card
-        src={data.product08.image}
-        cost={data.product08.cost}
-        title={data.product08.title}
-      />
-      <Card
-        src={data.product09.image}
-        cost={data.product09.cost}
-        title={data.product09.title}
-      />
+    <main className="ProductList">
+      {data.map((item) => {
+        return <Card src={item.image} cost={item.cost} title={item.title} />;
+      })}
     </main>
   );
 }
