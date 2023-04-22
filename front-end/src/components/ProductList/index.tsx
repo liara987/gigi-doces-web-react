25bf9efc9629400494682265e33e0837a5b9152c
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2023-04-21 21:13:41 -0300

    fix: Adding key to map

--- front-end/src/components/ProductList/index.tsx
+++ front-end/src/components/ProductList/index.tsx
@@ -5,8 +5,10 @@ import Card from "../Card/index.tsx";
 function ProductList() {
   return (
     <main className="ProductList">
-      {data.map((item) => {
-        return <Card src={item.image} cost={item.cost} title={item.title} />;
+      {data.map((item, i) => {
+        return (
+          <Card key={i} src={item.image} cost={item.cost} title={item.title} />
+        );
       })}
     </main>
   );
