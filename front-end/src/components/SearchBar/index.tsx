f882480d8c6b9e3716d5386f915a490df3615f8c
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2023-04-25 14:26:01 -0300

    chore: putting icon in separete folder

--- front-end/src/components/SearchBar/index.tsx
+++ front-end/src/components/SearchBar/index.tsx
@@ -1,34 +1,11 @@
 import "./styles.scss";
+import iconSearch from "../../assets/icons/search-icon.svg";
 
 function SearchBar() {
   return (
     <div className="SearchBar">
       <input className="SearchBar-input" type="text" placeholder="Pesquisar" />
-      <svg
-        className="SearchBar-icon"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          cx="11.059"
-          cy="11.0586"
-          r="7.06194"
-          stroke="#737380"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M20.0034 20.0033L16.0518 16.0517"
-          stroke="#737380"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+      <img className="SearchBar-icon" src={iconSearch} alt="Icone de lupa" />
     </div>
   );
 }
